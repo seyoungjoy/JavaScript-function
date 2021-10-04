8d2115613e23b15bdb93d72f8fe9e8d12d51db63
seyoungjoy <diddpwl80@gmail.com>
2021-10-04 19:30:08 +0900

    slider practice

--- tab menu/tab1.js
+++ tab menu/tab1.js
@@ -1,56 +1,36 @@
-//1 content 내용이 안보이게 한다.
-//2 
-
-const targetLink = document.querySelectorAll('.tab-menu >li a');
-const tabContent = document.querySelectorAll('.tab-content > div');
-
-for(i=0; i<targetLink.length; i++){
-    targetLink[i].addEventListener('click', function(e){
-        e.preventDefault();
-        
-        const orgTarget = e.target.getAttribute('href');
-        const tabTarget = orgTarget.replace('#', '');
-        
-        for(x=0; x<tabContent.length; x++){
-            tabContent[x].classList.remove('active');
-            document.getElementById(tabTarget).classList.add('active');
-        }
-
-        for(j=0; j<targetLink.length; j++){
-            targetLink[j].classList.remove('active');
-            e.target.classList.add('active');
-        }
-    });
-}
+/*
+클릭 이벤트
+*/
 
 
-// var targetLink = document.querySelectorAll('.tab-menu a');
-// var tabContent = document.querySelectorAll('#tab-content > div');
-// console.log(tabContent);
 
-// for (var i = 0; i < targetLink.length; i++) {
-//     targetLink[i].addEventListener('click', function (e) {
+
+
+
+
+// const targetLink = document.querySelectorAll('.tab-menu >li a');
+// const tabContent = document.querySelectorAll('.tab-content > div');
+
+// for(i=0; i<targetLink.length; i++){
+//     targetLink[i].addEventListener('click', function(e){
 //         e.preventDefault();
-//         var orgTarget = e.target.getAttribute('href');
-//         // console.log(orgTarget);
-
-//         var tabTarget = orgTarget.replace('#', '');
-
-//         for(var x = 0; x < tabContent.length; x++){
-//             tabContent[x].style.display = 'none';
-//         }
-//         document.getElementById(tabTarget).style.display = 'block';
         
-//         for( var j = 0; j < targetLink.length; j++ ){
+//         const orgTarget = e.target.getAttribute('href');
+//         const tabTarget = orgTarget.replace('#', '');
+        
+//         for(x=0; x<tabContent.length; x++){
+//             tabContent[x].classList.remove('active');
+//             document.getElementById(tabTarget).classList.add('active');
+//         }
+
+//         for(j=0; j<targetLink.length; j++){
 //             targetLink[j].classList.remove('active');
 //             e.target.classList.add('active');
 //         }
-
 //     });
 // }
-// document.getElementById('tabs-1').style.display = 'block';
 
-// var orgTarget = a.replace('b','c');
+
 
 /*
 - addEventListener()
