b8b3eed8e941ef82e4fbc44ca44b10bcad06b8fe
seyoungjoy <diddpwl80@gmail.com>
2021-10-07 09:29:54 +0900

    todolist practice

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 
 ## TodoList 만들기
 [유튜브 참고 링크](https://youtu.be/Ttf3CEsEwMQ)<br>
+
+### 원하는 form 복사하기
+### preventDefault
+폼이 제출되어지는 것을 방지한다.
+
+### document.createElement()<br>
+이 메서드는 tagname의 html 요소를 만들어 반환한다.
+```
+document.body.onload = addElement;
+
+function addElement(){
+    const newDiv = document.createElement("div");
+    const newContent = document.createTextNode("환영합니다!");
+}
+```
--- todolist/app.js
+++ todolist/app.js
@@ -2,3 +2,27 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event){
+    event.preventDefault(); //버튼 클릭시 기본 설정된 페이지 새로고침을 막아줌.
+    //Todo Div
+    const todoDiv = document.createElement("div");
+    const newContent = document.createTextNode("환영!");
+
+    todoDiv.appendChild(newContent);
+
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(todoDiv, currentDiv);
+
+}
+
+/*
+변수 선언
+이벤트 리스너로 클릭시 해당 함수 작동하도록함
+함수 설정
+*/
