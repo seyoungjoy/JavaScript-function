520a2669e07399df2b3e1632a11582a1ab895c4c
seyoungjoy <diddpwl80@gmail.com>
2021-10-11 19:45:48 +0900

    module

---
+++ module-pt/app.mjs
@@ -0,0 +1,6 @@
+// import {pi, square, Person} from './lib.mjs';
+// import * as lib from './lib.mjs';
+import { pi as PI, square as sq, Person as P} from './lib.mjs';
+console.log(PI);
+console.log(sq(10));
+console.log(new P('Lee'));
---
+++ module-pt/lib.mjs
@@ -0,0 +1,13 @@
+const pi = Math.PI;
+
+function square(x){
+	return x * x;
+}
+
+class Person{
+	constructor(name){
+    	this.name = name;
+    }
+}
+
+export default x 
