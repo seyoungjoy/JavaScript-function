ddb3f4f97d6b8ebc1911b6b88593b376740271c8
seyoungjoy <diddpwl80@gmail.com>
2021-12-05 17:01:05 +0900

    arrow function

--- arrow function/README.md
+++ arrow function/README.md
@@ -1,16 +1,19 @@
 # Errow Function
+## 화살표 함수
+본문이 한 줄인 함수를 작성할 때 유용하다.  
+<br>
 ## 문법
 ```
 let func = (arg1, arg2) => expression
 ```
-  
+<br>
 위의 표현은 아래 함수의 축약 버전이라고 할 수 있다.
 ```
 let func = function(arg1, arg2){
     return expression;
 };
 ```
-  
+<br>
 인수가 하나밖에 없다면 인수를 감싸는 괄호를 생략할 수 있다.
 ```
 // let double = function(n){
--- arrow function/main.js
+++ arrow function/main.js
@@ -10,5 +10,28 @@
 //     return n * 2;
 // }
 
-let double = n => n*2;
-alert(double(4));
+// let double = n => n*2;
+// alert(double(4));
+
+// let age = prompt("나이를 알려주세요.", 18);
+
+// // let welcome = (age<18) ?
+// //     () => alert('안녕') : 
+// //     () => alert('안녕하세요!');
+// // welcome();
+
+// let welcome = function(age){
+//     if(age<18){
+//         alert('안녕');
+//     } else{
+//         alert('안녕하세요');
+//     }
+// }
+
+// welcome();
+
+let sum = (a, b) => {
+    let result = a+b;
+    return result;
+};
+alert(sum(1,2));
